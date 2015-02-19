9ea9f9ca0cd74c841a97fc7c561fdaaf4104ad31
James McPherson <jpmcpherson5@gmail.com>
2015-02-19 17:45:31 +1100

    Conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  root "pages#home"
-  get "about" => "pages#about"
-  get "asshole" => "pages#asshole"
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
